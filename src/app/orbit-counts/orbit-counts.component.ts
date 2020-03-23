08c3a12fe4f03c7dd3dcf6bb4963ce5461d117e2
bwyard <45057171+bwyard@users.noreply.github.com>
2020-03-23 18:34:50 -0500

    final update

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+@Input() satellites: Satellite[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+  countType(arrayList,satType){
+    let total = 0
+    for (let arrayIndex in arrayList){
+      //console.log(arrayList[arrayIndex])
+      if (arrayList[arrayIndex].type.toLowerCase() === satType.toLowerCase()){
+        total += 1
+      }
+    }
+    return total;
+
+  }
+
+}
